12718125e4eac6b5048d7f7ee43f50543a34f418
Raj Sandhu <rajsandhu@gmail.com>
2019-10-29 12:02:40 +0100

    added simple card, complex card, and beginning of graph issue: the graph isn't rendering although the space for it does

--- src/MyGrid.js
+++ src/MyGrid.js
@@ -1,3 +1,6 @@
+// source: https://material-ui.com/components/grid/
+
+
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
